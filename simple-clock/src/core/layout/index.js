6c2a884eaca283818b46e3b801bd1b325f865b67
manohar.v@nyros.com <manohar.v@nyros.com>
2024-02-15 10:52:18 +0530

    Trigger rebuild

--- simple-clock/src/core/layout/index.js
+++ simple-clock/src/core/layout/index.js
@@ -16,7 +16,7 @@ export const Layout = (props) => {
             <div className="flex-grow-1">
               <div className="ReactTitle ps-3  d-flex align-items-center">
                 <img src={require("../../react-2@2x.png")} alt="logo" />
-                <h3 className="mb-0">Reactjs Apps with APIs</h3>
+                <h3 className="mb-0">Reactjs Apps</h3>
               </div>
               <div className="d-flex ps-3 align-items-center">
                 <div className="flex-grow-1">
